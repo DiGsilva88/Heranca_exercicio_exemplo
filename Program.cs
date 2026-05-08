7eebadc89d73b8a9fc8f42e9c23027d28d1b1b55
DianaS <diana.silva.257306188@msft.cesae.pt>
2026-05-08 11:24:27 +0100

    Demonstração do crescimento do sistema a partir de listas

--- Program.cs
+++ Program.cs
@@ -5,47 +5,110 @@
         static void Main(string[] args)
         {
 
-            //implementar a classe base-funcionario-check
+            List<Funcionario> listaFuncionario = new List<Funcionario>();
 
-            Funcionario f1 = new Funcionario("Luís", 45, "Porto");
-            Funcionario f2 = new Funcionario("Sara", 42, "Braga");
+            Funcionario f1 = new Funcionario("Luis", 45, "Barcelos");
+            Funcionario socio = new Socio("Pedro", 34, "Braga",100);
+            Funcionario tarefeiro = new Outsourcing("maria", 28, "Viana",15);
 
+            //adiconar lista
+            listaFuncionario.Add(f1);
+            listaFuncionario.Add(socio);
 
-            //polimorfismo de referencia
-            //criar o scoio chamando os construtores
-            Funcionario socio1 = new Socio("Manuel", 35, "Vila Verde", 100);
+            listaFuncionario.Add(tarefeiro);
 
+            foreach (Funcionario f in listaFuncionario) 
 
-            //do lado esquerdo o compilador ve o socio como um funcionario
-            //new socio=> o objecto real é do tipo Socio
-            //ou seja é um funcionario especializado
-
-            Socio socio2 = new Socio("Pedro", 38, "Braga", 200);
-
-            f1.ExibirInformacoes();
-            socio1.ExibirInformacoes();
-            socio2.MostrarqtAccoes();
-            socio2.ExibirInformacoes();
+            {
+                f.ExibirInformacoes();
 
 
-            //o scoio também é um funcionario só que é especializado
+                
+            }
 
-            Funcionario socio3 = new Socio("João", 35, "Braga", 300);
-            //chamo o construtor mas é um funcionario
+            Socio s = new Socio("asd", 34, "brg", 500);
+            listaFuncionario.Add(s);
 
-            //polimorfismo vem do grego multiplas formas
+            foreach (Funcionario f in listaFuncionario)
 
-            //se quero acrescentar algum comportamento a mais vou escrever por exemplo (nAccoes=x)
-
-
-            //criar o  funcionario tarefeiro de Outsourcing
-
-            Funcionario tarefeiro = new Outsourcing("Maria", 27, "Braga", 20);
-            Console.WriteLine("Exibi a informação do tarefeiro");
-            tarefeiro.ExibirInformacoes();
+            {
+                f.ExibirInformacoes();
 
 
-            //com a herança só acresce e pode ser reutilizavel,só cresce as classes ,herdam os metodos
+
+            }
+
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //    //implementar a classe base-funcionario-check
+
+        //    Funcionario f1 = new Funcionario("Luís", 45, "Porto");
+        //    Funcionario f2 = new Funcionario("Sara", 42, "Braga");
+
+
+        //    //polimorfismo de referencia
+        //    //criar o scoio chamando os construtores
+        //    Funcionario socio1 = new Socio("Manuel", 35, "Vila Verde", 100);
+
+
+        //    //do lado esquerdo o compilador ve o socio como um funcionario
+        //    //new socio=> o objecto real é do tipo Socio
+        //    //ou seja é um funcionario especializado
+
+        //    Socio socio2 = new Socio("Pedro", 38, "Braga", 200);
+
+        //    f1.ExibirInformacoes();
+        //    socio1.ExibirInformacoes();
+        //    socio2.MostrarqtAccoes();
+        //    socio2.ExibirInformacoes();
+
+
+        //    //o scoio também é um funcionario só que é especializado
+
+        //    Funcionario socio3 = new Socio("João", 35, "Braga", 300);
+        //    //chamo o construtor mas é um funcionario
+
+        //    //polimorfismo vem do grego multiplas formas
+
+        //    //se quero acrescentar algum comportamento a mais vou escrever por exemplo (nAccoes=x)
+
+
+        //    //criar o  funcionario tarefeiro de Outsourcing
+
+        //    Funcionario tarefeiro = new Outsourcing("Maria", 27, "Braga", 20);
+        //    Console.WriteLine("Exibi a informação do tarefeiro");
+        //    tarefeiro.ExibirInformacoes();
+
+
+        //    //com a herança só acresce e pode ser reutilizavel,só cresce as classes ,herdam os metodos
+        //}
     }
-}
+    }
