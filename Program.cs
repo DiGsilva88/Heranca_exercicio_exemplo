672da52424ede21035a26cda8afe9d8bdc246036
DianaS <diana.silva.257306188@msft.cesae.pt>
2026-05-08 09:37:21 +0100

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Heranca_exercicio_exemplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //implementar a classe base-funcionario
+
+
+
+
+        }
+    }
+}
